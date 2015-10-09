63a2a025e57900713d1ae6c74c233eb395c5a800
Nils Blum-Oeste <nils.blum-oeste@lambdawerk.com>
2015-10-09 22:24:10 +0200

    Add circle collision

--- src/bouncing_sounds/core.clj
+++ src/bouncing_sounds/core.clj
@@ -3,28 +3,56 @@
              [core :as q]
              [middleware :as m]]))
 
-(defn reset []
+(defn reset-state []
   {:circles []})
 
 (defn setup []
   (q/smooth)
   (q/frame-rate 10)
+  (q/no-fill)
   (q/color-mode :hsb 100 100 100)
-  (reset))
+  (reset-state))
+
+(defn colliding? [{x1 :x y1 :y r1 :radius}
+                  {x2 :x y2 :y r2 :radius}]
+  (when-not (= 0 (- x1 x2) (- y1 y2))
+    (< (q/dist x1 y1 x2 y2)
+       (+ r1 r2))))
+
+(defn collides-with-any? [circle other-circles]
+  (some #{true} (map #(colliding? circle %) other-circles)))
+
+(defn kill-small [circles]
+  (remove #(< (:radius %) 5) circles))
+
+(defn collide-circles [circles]
+  (map #(if (collides-with-any? % circles)
+          (assoc % :grow? false)
+          %)
+       circles))
+
+(defn grow-circles [circles]
+  (map #(update % :radius (if (:grow? %) inc dec)) circles))
 
 (defn update-state [state]
-  (update state :circles (fn [circles] (map #(update % :radius inc) circles))))
+  (-> state
+      (update :circles kill-small)
+      (update :circles collide-circles)
+      (update :circles grow-circles))
+  #_
+  (reset-state))
 
 (defn draw-state [{:keys [circles]}]
   (q/background 0)
-  (q/no-fill)
+
   (doseq [{:keys [x y radius hue]} circles]
     (q/stroke hue 100 100)
-    (q/ellipse x y radius radius)))
+    (q/ellipse x y (* 2 radius) (* 2 radius))))
 
 (defn mouse-clicked [state {:keys [x y]}]
-  (update state :circles conj {:x x :y y :radius 20 :hue (rand-int 100)}))
+  (update state :circles conj {:x x :y y :radius 20 :hue (rand-int 100) :grow? true}))
 
+#_
 (q/defsketch bouncing-sounds
   :title "Bouncing Sounds"
   :size [800 800]
